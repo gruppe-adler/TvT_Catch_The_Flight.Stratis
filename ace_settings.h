806c70736acb82af6673296666a0787ca9a416b1
Moritz Schmidt <fusselwurm@gmail.com>
2016-12-18 20:39:32 +0100

    fix ace settings (#27, #9)

---
+++ ace_settings.h
@@ -0,0 +1,40 @@
+class ace_settings {
+    class ace_winddeflection_enabled {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+    class ace_winddeflection_vehicleEnabled {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+
+    class ace_map_BFT_Enabled {
+        value = 0;
+        typeName = "BOOL";
+        force = 1;
+    };
+
+    class ace_spectator_filterUnits {
+        value = 3;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_spectator_filterSides {
+        value = 3;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_spectator_restrictModes {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    class ace_spectator_restrictVisions {
+        value = 0;
+        typeName = "SCALAR";
+        force = 1;
+    };
+    
+};
