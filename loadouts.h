b23349d1bbfc715ccb71eec2b4cb97fc517aa29d
Moritz Schmidt <fusselwurm@gmail.com>
2016-12-18 12:57:03 +0100

    loads of loadout fixes, should fix #10

--- loadouts.h
+++ loadouts.h
@@ -7,7 +7,7 @@ class Loadouts {
     class AllUnits {
         addItemsToUniform[] = {
             "ACE_Flashlight_KSF1",
-            LIST_5("ACE_fieldDressing")
+            LIST_6("ACE_fieldDressing")
         };
         map = "ItemMap";
         compass = "ItemCompass";
@@ -29,44 +29,88 @@ class Loadouts {
                 vest = "rhsgref_6b23_ttsko_mountain_rifleman";
                 headgear = "rhsgref_ssh68_ttsko_mountain";
                 radio = "tf_fadak";
-                addItemsToVest[] = {
-                    LIST_2("SmokeShell"),
-                    LIST_2("Chemlight_green")
+                addItemsToUniform[] = {
+                    LIST_1("ACE_epinephrine"),
+                    LIST_1("ACE_morphine")
                 };
             };
         };
 
-        class OPF_G_F: CIV_F {
-            class AllUnits: AllUnits {};
+        class OPF_G_F {
+            class AllUnits  {
+                uniform = "rds_uniform_Woodlander1";
+                addItemsToUniform[] = {
+                    LIST_1("ACE_SpraypaintRed"),
+                    "SmokeShell"
+                };
+            };
         };
 
-        class IND_F: CIV_F {
-            class AllUnits: AllUnits {};
+        class IND_F {
+            class AllUnits {
+                uniform = "rds_uniform_Woodlander1";
+            };
+        };
+
+        class IND_G_F {
+            class AllUnits  {
+                uniform = "rds_uniform_Woodlander1";
+                addItemsToUniform[] = {
+                    LIST_1("ACE_SpraypaintRed"),
+                    "SmokeShell"
+                };
+            };
         };
     };
 
     class Type {
         class O_Soldier_F {
             primaryWeapon = "arifle_AKS_F";
-            primaryWeaponAttachments[] = {"acc_flashlight"};
             addItemsToVest[] = {
-                LIST_5("30Rnd_545x39_Mag_F")
+                LIST_6("30Rnd_545x39_Mag_F")
             };
         };
-        class O_Soldier_TL: O_Soldier_F {
+        class O_Soldier_TL_F {
             binoculars = "Binocular";
+            primaryWeapon = "arifle_AKS_F";
             addItemsToVest[] = {
-                LIST_5("UGL_FlareGreen_F")
+                LIST_1("ACE_SpraypaintRed"),
+                LIST_2("SmokeShell"),
+                LIST_4("30Rnd_545x39_Mag_F")
             };
         };
-        class O_Soldier_SL: O_Soldier_TL {
+        class O_Soldier_SL_F {
+            binoculars = "Binocular";
+            primaryWeapon = "arifle_AKS_F";
+            addItemsToVest[] = {
+                LIST_1("ACE_SpraypaintRed"),
+                LIST_4("30Rnd_545x39_Mag_F"),
+                LIST_2("SmokeShell"),
+                LIST_2("SmokeShellGreen"),
+                LIST_2("SmokeShellRed")
+            };
         };
-        class O_Officer_F: O_Soldier_SL {
-        };
+        class O_Officer_F: O_Soldier_SL_F {};
         class O_Soldier_AR_F {
             primaryWeapon = "rhs_weap_pkm";
             addItemsToVest[] = {
-                "rhs_100Rnd_762x54mmR_green"
+                LIST_1("rhs_100Rnd_762x54mmR_green")
+            };
+        };
+        class O_medic_F: O_Soldier_F {
+            backpack = "rhs_assault_umbts";
+            addItemsToBackpack[] = {
+                LIST_10("ACE_fieldDressing"),
+                LIST_10("ACE_fieldDressing"),
+                LIST_4("ACE_fieldDressing"),
+                LIST_5("ACE_epinephrine"),
+                LIST_10("ACE_morphine")
+            };
+        };
+        class O_Soldier_AAR_F: O_Soldier_F {
+            backpack = "rhs_assault_umbts";
+            addItemsToBackpack[] = {
+                LIST_3("rhs_100Rnd_762x54mmR_green")
             };
         };
         class O_Soldier_LAT_F: O_Soldier_F {
@@ -81,16 +125,9 @@ class Loadouts {
             handgunWeapon = "rhs_weap_makarov_pm";
             binoculars = "Binocular";
             addItemsToUniform[] = {
-                LIST_2("Chemlight_green"),
                 LIST_2("rhs_mag_9x18_8_57N181S")
             };
             headgear = "H_Hat_checker";
-        };
-
-        class I_Survivor_F {
-            addItemsToUniform[] = {
-                "Chemlight_green"
-            };
         };
     };
 };
--- loadouts.sqf
+++ loadouts.sqf
@@ -1,6 +1,6 @@
 [
     {
-        _value = param [0];
+        params ["_value", "_unit"];
         if (_value == "rds_uniform_Woodlander1") then {
             _value = selectRandom [
                 "rds_uniform_Worker1",
@@ -29,3 +29,22 @@
     },
     "uniform"
 ] call GRAD_Loadout_fnc_addReviver;
+
+_lessSprayPaint = {
+    params ["_value", "_unit"];
+
+    ([_value, {
+        _reject = false;
+        if (_this == "ACE_SpraypaintRed") then {
+            if (faction _unit == "IND_G_F" || faction _unit == "OPF_G_F") then {
+                _reject = (random 1) >= 0.5;
+            };
+        };
+
+        _reject
+    }] call CBA_fnc_reject);
+};
+
+[_lessSprayPaint, "addItemsToUniform"] call GRAD_Loadout_fnc_addReviver;
+[_lessSprayPaint, "addItemsToVest"] call GRAD_Loadout_fnc_addReviver;
+[_lessSprayPaint, "addItemsToBackpack"] call GRAD_Loadout_fnc_addReviver;
